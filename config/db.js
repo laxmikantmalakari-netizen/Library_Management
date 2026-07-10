55189028df2e3e5392665bf6e72d21659e1f7fc2
laxmikantmalakari-netizen <laxmikantmalakari@gmail.com>
2026-07-10 15:55:33 +0530

    Refactor connectDB function for better logging
    Improved error handling and logging for MongoDB connection.

--- config/db.js
+++ config/db.js
@@ -1,13 +1,19 @@
-const mongoose=require("mongoose");
-const connectDB=async()=>{
-    try{
-        console.log("connecting to mongodb...");
-        await mongoose.connect(ProcessingInstruction.env.MONGO_URI);
-        console.log("MongoDB connected successfully");
-        console.log("Database :",mongoose.connection.name);
-        console.log("Host:",mongoose.connection.host);
+const mongoose = require("mongoose");
+
+const connectDB = async ()=> {
+    try  {
+        console.log("Connecting to mongoDB...");
+
+        await mongoose.connect(process.env.MONGO_URI);
+
+        console.log("MongoDB Conneceted Successfully");
+        console.log("Database  :",mongoose.connection.name);
+        console.log("Host   :",mongoose.connection.host);
+    
     }
     catch(error){
-        console.log("")
+        console.log("MongoDB Connection failed");
+        console.log(error.message);
     }
 }
+module.exports = { connectDB };
